08868d459309f2e7ae3733d3f2832e3a61152baf
tuheen66 <hassan.monirul@gmail.com>
2024-10-15 10:14:40 +0600

    new commit in public repo

--- src/Components/AllBrands/BMW.jsx
+++ src/Components/AllBrands/BMW.jsx
@@ -14,22 +14,22 @@ const BMW = () => {
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={bmws[0].image} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
+                        <a href="#slide3" className="btn btn-circle"></a>
+                        <a href="#slide2" className="btn btn-circle"></a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
                     <img src={bmws[1].image} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
+                        <a href="#slide1" className="btn btn-circle"></a>
+                        <a href="#slide3" className="btn btn-circle"></a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
                     <img src={bmws[2].image} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
+                        <a href="#slide2" className="btn btn-circle"></a>
+                        <a href="#slide1" className="btn btn-circle"></a>
                     </div>
                 </div>
             </div>
